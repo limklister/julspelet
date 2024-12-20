61583da0217f5f01f0662d931eb4818dc07c7313
Alexander Lindquister <alexander.lindquister@nordnet.se>
2024-12-20 17:01:43 +0100

    style: image for package

--- js/package.js
+++ js/package.js
@@ -2,10 +2,14 @@ class Package {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.width = 20;
-        this.height = 20;
+        this.width = 30; // Adjusted for image
+        this.height = 30; // Adjusted for image
         this.velocityY = 0;
         this.collected = false;
+        
+        // Load the gift image
+        this.image = new Image();
+        this.image.src = 'images/gift.png';
     }
 
     update() {
@@ -17,8 +21,14 @@ class Package {
 
     draw(ctx) {
         if (!this.collected) {
-            ctx.fillStyle = '#00FF00'; // Green for packages
-            ctx.fillRect(this.x - this.width/2, this.y, this.width, this.height);
+            // Draw the gift image instead of a green rectangle
+            ctx.drawImage(
+                this.image, 
+                this.x - this.width/2, 
+                this.y, 
+                this.width, 
+                this.height
+            );
         }
     }
 
